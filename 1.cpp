102a74631b236cf1b3126d9e4148755a680c75d4
Haze-dev1 <devanshpalsapure11@google.com>
2026-02-16 21:56:12 +0530

    day1

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+    int home = 8, one = 4, two = 4, three = 3;
+    cout << "Home: " << home << " people\n";
+    cout << one << " stayed, " << two << " swept away\n";
+    int total = one + two + three;
+    cout << "All together: " << total << " people\n";
+    return 0;
+}
